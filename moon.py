36156c28dad066a34787cbef47e252a9e9cfd8be
Tekalig <tekahazi06@gmail.com>
2025-02-12 10:08:28 +0300

    implement the use of jinja in data structure

---
+++ moon.py
@@ -0,0 +1,6 @@
+class Moons:
+    def __init__(self, first, second, third, fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
